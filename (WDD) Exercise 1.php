36a7fe48618dc94b7ba959bdd104325b38fd42bf
Vince Villasor <78065232+vincev16@users.noreply.github.com>
2022-09-30 16:22:56 +0800

    Created using Notepad ++ (September 30)

---
+++ (WDD) Exercise 1.php
@@ -0,0 +1,33 @@
+<html>
+<head><title> Exercise 1 - WDD </title></head>
+<body bgcolor = "lightgreen">
+
+	<h1> Exercise #1 </h1>
+	
+<?php
+
+	$FullName = 'Vince Kazer M. Villasor';
+	$CollegeProgram = 'BS in Computer Engineering';
+	$Year = '3rd Year';
+	$Section = 'Web Design Development Lab - 58043';
+	$String = 'Today, I will learn PHP Programming.';
+	
+	
+	echo "My Full Name is ".$FullName;
+	echo "<br>";
+	echo "<br>";
+	echo "Taking the program ".$CollegeProgram;
+	echo "<br>";
+	echo "<br>";
+	echo "Currently in ".$Year." Level";
+	echo "<br>";
+	echo "<br>";
+	echo "In Section, ".$Section;
+	echo "<br>";
+	echo "<br>";
+	echo $String;
+
+?>	
+
+</body>
+</html>
